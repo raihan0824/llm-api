53922e000fe392d83d928533954e5b4ecad11c08
Raihan <raihanafiandi@email.com>
2023-11-01 08:54:50 +0000

    add paralellism and argparse

--- server/routes/main_services.py
+++ server/routes/main_services.py
@@ -1,29 +1,62 @@
 from fastapi import APIRouter
-from server.utils.pipeline import Pipeline
+from server.utils.pipeline import PipelineProcess
 from pydantic import BaseModel
 from typing import Optional,List
 import os
+from dotenv import load_dotenv
+from concurrent.futures import ThreadPoolExecutor
+from typing import List
+from math import ceil
+import argparse
+
+# Create the parser
+parser = argparse.ArgumentParser(description="Process some integers.")
+
+# Add the arguments
+parser.add_argument('--num_gpu', type=int, default=8, help='The number of GPUs')
+
+args = parser.parse_args()
+
+
+load_dotenv()
 
 class Item(BaseModel):
     prompts: List[str]
     model: str
+    temperature: Optional[float] = 0.9
     top_p: Optional[float] = 0.5
     top_k: Optional[int] = 40
+    frequency_penalty: Optional[float] = 0
+    presence_penalty: Optional[float] = 0
 
-model_path_bloom = os.getenv("PATH_BLOOM")
-# model_path_llama = 'models/llama_40k_inst'
-bloom_pipeline = Pipeline(model_path_bloom)
-# llama_pipeline = Pipeline(model_path_llama)
+model_path_bloom = "../trl-llama/models/bloom/bloom_synthetic_40k_v2"
+
+# pipelines = [PipelineProcess(model_path_bloom, i) for i in range(int(os.getenv("NUM_GPUS","8")))]
+pipelines = [PipelineProcess(model_path_bloom, i) for i in range(args.num_gpu)]
+# pipelines = [PipelineProcess(model_path_bloom, i) for i in range(4,8)]
+# pipelines = [PipelineProcess(model_path_bloom, i) for i in range(4)] + [PipelineProcess("../trl-llama/models/rlhf/v3/v3step_620_merged", i) for i in range(4,8)]
 
 generation_router=APIRouter(tags=['Generate response'])
-
 @generation_router.post("/api/v1/generate/")
 async def generate(item: Item):
     model = item.model
     top_p = item.top_p
     top_k = item.top_k
-    if model=="bloom":
-        response = bloom_pipeline.generate(prompts=item.prompts,top_p=top_p,top_k=top_k)
-    elif model=="llama":
-        response = bloom_pipeline.generate(prompts=item.prompts,top_p=top_p,top_k=top_k)
+    temperature = item.temperature
+    frequency_penalty = item.frequency_penalty
+    presence_penalty = item.presence_penalty
+
+    if model in ["bloom", "bloom_2"]:
+        # Calculate the size of each chunk
+        chunk_size = ceil(len(item.prompts) / len(pipelines))
+        # Split the prompts into chunks
+        chunks = [item.prompts[i:i + chunk_size] for i in range(0, len(item.prompts), chunk_size)]
+
+        with ThreadPoolExecutor(max_workers=len(pipelines)) as executor:
+            futures = [executor.submit(pipeline.generate, chunk, temperature, top_p, top_k,presence_penalty,frequency_penalty) for pipeline, chunk in zip(pipelines, chunks)]
+            responses = [future.result() for future in futures]
+        response = sum(responses, [])  # Concatenate all responses
+    else:
+        response = "Invalid model"
+
     return {"response": response}
--- server/utils/pipeline.py
+++ server/utils/pipeline.py
@@ -3,12 +3,17 @@ from server.utils.prompter import Prompter
 from pydantic import BaseModel
 from typing import Optional,List
 from vllm import LLM,SamplingParams
+from multiprocessing import Process, Queue
+import os
 
 class Item(BaseModel):
     prompts: List[str]
     model: str
+    temperature: Optional[float] = 0.9
     top_p: Optional[float] = 0.5
     top_k: Optional[int] = 40
+    frequency_penalty: Optional[float] = 0
+    presence_penalty: Optional[float] = 0
 
 class Pipeline(object):
 
@@ -20,17 +25,47 @@ class Pipeline(object):
         self.load_model()
 
     def load_model(self):
-        self.llm = LLM(self.model_path, tensor_parallel_size=4,gpu_memory_utilization=0.8)
+        self.llm = LLM(self.model_path, 
+                       tensor_parallel_size=1,
+                       gpu_memory_utilization=0.9
+                    #    ,max_num_seqs=1280
+                       )
 
-    def generate(self,prompts,top_p=0.5,top_k=40):
+    def generate(self,prompts,temperature=0.9,top_p=0.5,top_k=40,presence_penalty=0,frequency_penalty=0):
         outputs = self.llm.generate(
             prompts,
-            sampling_params=SamplingParams(temperature=0.8, top_p=top_p,top_k=top_k,max_tokens=128)
+            sampling_params=SamplingParams(temperature=temperature, top_p=top_p,top_k=top_k,max_tokens=128,presence_penalty=presence_penalty,frequency_penalty=frequency_penalty)
         )
         responses=[]
-        for output in outputs:
+        for prompt, output in zip(prompts, outputs):
             generated_text = output.outputs[0].text
             response = self.prompter.get_response(generated_text)
             responses.append(response)
+            # print((prompt, response))
         torch.cuda.empty_cache()
         return responses
+    
+class PipelineProcess:
+    def __init__(self, model_path, device):
+        self.queue = Queue()
+        self.process = Process(target=self.run, args=(model_path, device, self.queue))
+        self.process.start()
+
+    def run(self, model_path, device, queue):
+        os.environ["CUDA_VISIBLE_DEVICES"] = str(device)
+        pipeline = Pipeline(model_path)
+        while True:
+            item = queue.get()
+            if item is None:  # We send None to indicate the process should terminate
+                break
+            prompts, temperature, top_p, top_k,presence_penalty,frequency_penalty = item
+            response = pipeline.generate(prompts=prompts,temperature=temperature, top_p=top_p, top_k=top_k,frequency_penalty=frequency_penalty,presence_penalty=presence_penalty)
+            queue.put(response)
+
+    def generate(self, prompts, temperature, top_p, top_k,presence_penalty,frequency_penalty):
+        self.queue.put((prompts, temperature, top_p, top_k,presence_penalty,frequency_penalty))
+        return self.queue.get()
+
+    def terminate(self):
+        self.queue.put(None)
+        self.process.join()
